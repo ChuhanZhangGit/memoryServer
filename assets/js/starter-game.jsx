20cfb804d46710cf823d48e1554f6b60e2ced924
chuhan95 <zhang.chuh@husky.neu.edu>
2019-09-25 01:05:31 -0400

    first try

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -3,45 +3,152 @@ import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
 export default function game_init(root) {
-  ReactDOM.render(<Starter />, root);
+  ReactDOM.render(<Memory />, root);
 }
 
-class Starter extends React.Component {
+// Fisher–Yates shuffle algorithm
+function shuffle(array) {
+  for (let i = 0; i < array.length; i++) {
+    let rand = Math.floor(Math.random() * i);
+    let temp = array[rand];
+    array[rand] = array[i];
+    array[i] = temp;
+  }
+  return array;
+}
+
+class Memory extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { left: false };
+    const slotState = Object.freeze({
+      HIDDEN: "hidden",
+      CLICKED: "clicked",
+      REVEALED: "revealed"
+    })
+    let charArray = "ABCDEFGH".repeat(2).split("");
+    charArray = charArray.map(x => [x, "hidden"])
+    this.state = {
+      slotArray: shuffle(charArray),
+      left: false,
+    };
   }
 
-  swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
-    this.setState(state1);
+  click_hidden(id_num) {
+    let temp_slots = this.state.slotArray;
+    var other_idx;
+    var other_state;
+    for (let i = 0; i < 16; i++) {
+      if (i != id_num && temp_slots[i][0] === temp_slots[id_num][0]) {
+        other_idx = i;
+        other_state = temp_slots[i][1];
+      }
+    }
+    if (other_state === "clicked") {
+      temp_slots[id_num][1] = "revealed";
+      temp_slots[other_idx][1] = "revealed";
+    }
+    else if (other_state === "hidden") {
+      temp_slots[id_num][1] = "clicked";
+      window.setTimeout(() => {
+        this.button_time_out(id_num);
+      }, 1000);
+    }
+    let temp_state = _.assign({}, this.state, { slotArray: temp_slots });
+    this.setState(temp_state);
+
+    // why must use bind to pass "this" to current function.
   }
 
-  hax(_ev) {
-    alert("hax!");
+  button_time_out(id_num) {
+    let temp_slots = this.state.slotArray;
+    temp_slots[id_num][1] = "hidden";
+    let temp_state = _.assign({}, this.state, { slotArray: temp_slots });
+    this.setState(temp_state);
   }
+
+  restart(_ev) {
+    let temp_slots = this.state.slotArray;
+    temp_slots = shuffle(temp_slots.map(element => [element[0], "hidden"]));
+    let temp_state = _.assign({}, this.state, { slotArray: temp_slots });
+    this.setState(temp_state);
+  }
+
 
   render() {
-    let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
+    let table = [];
+    for (let i = 0; i < 4; i++) {
+      let row = []
+      for (let j = 0; j < 4; j++) {
+        let id_num = i * 4 + j;
+        let id_str = id_num.toString();
+        row.push(this.getButton(id_num));
+      }
+      table.push(<div key={"row" + i} className="row">{row}</div>);
+    }
+    return (
+      <div>
+        {table}
+        <div>
+          <Restart_button root={this} />
+        </div>
+      </div>);
+  }
 
-    let blank = <div className="column">
-      <p>Nothing here.</p>
+  getButton(id_num) {
+    let reveal = <div key={id_num} className="column">
+      <button>
+        {this.state.slotArray[id_num][0]}
+      </button>
     </div>;
-
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
+    let clicked = <div key={id_num} className="column">
+      <button>
+        {this.state.slotArray[id_num][0]}
+      </button>
+    </div>;
+    let hidden = <div key={id_num} className="column">
+      <button onClick={this.click_hidden.bind(this, id_num)}></button>
+    </div>;
+    if (this.state.slotArray[id_num][1] === "revealed") {
+      return reveal;
+    }
+    else if (this.state.slotArray[id_num][1] === "clicked") {
+      return clicked;
     }
     else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
+      return hidden;
     }
   }
 }
 
+function Restart_button(param) {
+  let root = param.root;
+  return (
+    <div>
+      <button onClick={root.restart.bind(root)}>
+        Restart
+        </button>
+    </div>
+  );
+}
+
+
+
+    // let button = <div className="column" onMouseMove={this.swap.bind(this)}>
+    //   <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
+    // </div>;
+
+    // let blank = <div className="column">
+    //   <p>Nothing here.</p>
+    // </div>;
+
+    // if (this.state.left) {
+    //   return <div className="row">
+    //     {button}
+    //     {blank}
+    //   </div>;
+    // }
+    // else {
+    //   return <div className="row">
+    //     {blank}
+    //     {button}
+    //   </div>;
